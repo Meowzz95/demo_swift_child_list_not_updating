a6a970b4560a68ff1077aba1512a6b464a1bb334
meowzz95 <22430379+Meowzz95@users.noreply.github.com>
2023-02-04 21:44:39 +0800

    Initial Commit

---
+++ test-list-pass-to-children/test_list_pass_to_childrenApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_list_pass_to_childrenApp.swift
+//  test-list-pass-to-children
+//
+//  Created by Mimimi on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct test_list_pass_to_childrenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
